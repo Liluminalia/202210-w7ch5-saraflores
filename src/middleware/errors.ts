5115f5ed24152d75017245bd509061cde4e3f994
Liluminalia <peloazul4@gmail.com>
2022-11-21 22:21:55 +0100

    updating again errors

--- src/middleware/errors.ts
+++ src/middleware/errors.ts
@@ -1,27 +1,23 @@
-import { Request, Response, NextFunction } from 'express';
-import { CustomError } from '../interfaces/error';
+import { CustomError } from '../interfaces/error.js';
+import { NextFunction, Request, Response } from 'express';
 
-export const errorManager = (
-    error: CustomError,
-    _req: Request,
-    resp: Response,
-    _next: NextFunction
-) => {
-    _next;
-    console.log(
-        error.name,
-        error.statusCode,
-        error.statusMessage,
-        error.message
-    );
-    let status = error.statusCode || 500;
-    if (error.name === 'ValidationError') {
-        status = 406;
-    }
-    const result = {
-        status: status,
-        type: error.name,
-        error: error.message,
+export const errorManager = () => {
+    (error: CustomError, _req: Request, resp: Response) => {
+        console.log(
+            error.name,
+            error.statusCode,
+            error.statusMessage,
+            error.message
+        );
+        let status = error.statusCode || 500;
+        if (error.name === 'ValidationError') {
+            status = 406;
+        }
+        const result = {
+            status: status,
+            type: error.name,
+            error: error.message,
+        };
+        resp.status(status).json(result).end();
     };
-    resp.status(status).json(result).end();
 };
