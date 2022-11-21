8d4417a7bb25e020a6a9f5f97bb58be6c12e8f05
Liluminalia <peloazul4@gmail.com>
2022-11-21 20:34:29 +0100

    updating app file for users

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-import { CustomError } from './interfaces/error.js';
 import { robotRouter } from './router/robots.router.js';
+import { errorManager } from './middleware/errors.js';
+import { setCors } from './middleware/cors.js';
+import { userRouter } from './router/user.router.js';
 
 export const app = express();
 app.use(cors());
@@ -13,33 +15,11 @@ const corsOptions = {
 app.use(morgan('dev'));
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use((req, res, next) => {
-    const origin = req.header('Origin') || '*';
-    res.setHeader('Access-Control-Allow-Origin', origin);
-    next();
-});
+app.use(setCors);
 
 app.get('/', (req, res) => {
     res.send('API de Robots, pon /robots al final de la URL').end();
 });
 app.use('/robots', robotRouter);
-app.use(
-    (error: CustomError, _req: Request, resp: Response, next: NextFunction) => {
-        console.log(
-            error.name,
-            error.statusCode,
-            error.statusMessage,
-            error.message
-        );
-        let status = error.statusCode || 500;
-        if (error.name === 'ValidationError') {
-            status = 406;
-        }
-        const result = {
-            status: status,
-            type: error.name,
-            error: error.message,
-        };
-        resp.status(status).json(result).end();
-    }
-);
+app.use('/users', userRouter);
+app.use(errorManager);
